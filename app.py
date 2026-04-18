bc4403d4ca9eae0c264bc145263622fddf16a88e
Avika Sachan <avikasachan0909@gmail.com>
2026-04-18 22:11:58 +0530

    chnaging the app.run

--- app.py
+++ app.py
@@ -105,4 +105,4 @@ def reset():
     return jsonify({'message': 'User not found or password unchanged.'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
